c52468e33362f73560a20ef1f77365c103a0e8fd
Kalaiselvan Thangavel <kalaiselvan.t@payoda.com>
2020-12-29 14:46:17 +0530

    digital magazine add/edit

--- src/app/pages/digital-magazine/digital-magazine-add-edit/digital-magazine-add-edit.component.ts
+++ src/app/pages/digital-magazine/digital-magazine-add-edit/digital-magazine-add-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from '../../../modules/auth/_services/apiservice.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-digital-magazine-add-edit',
@@ -6,10 +9,121 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./digital-magazine-add-edit.component.scss']
 })
 export class DigitalMagazineAddEditComponent implements OnInit {
+  public digitalMagazineFormFields: any = {
+    title: '',
+    description: '',
+    magazinePDF: File,
+    thumbnailImage: File
+  };
+  public supportedFiles: string[] = ['.pdf'];
+  public supportedImageFiles: string[] = ['.png', '.jpg', '.jpeg'];
 
-  constructor() { }
+  public digitalMagazineId = this.activeroute.snapshot.paramMap.get(
+    'digitalMagazineId'
+  );
+
+  constructor(
+    public apiservice: ApiserviceService,
+    public router: Router,
+    public activeroute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    if (this.digitalMagazineId) {
+      this._getDigitalMagazineById();
+    }
   }
 
+  public onClickSaveCategory(form) {
+    if (form.invalid) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('title', this.digitalMagazineFormFields.title);
+    formData.append('description', this.digitalMagazineFormFields.description);
+    formData.append(
+      'thumbnailImage',
+      this.digitalMagazineFormFields.thumbnailImage
+    );
+    formData.append('magazinePDF', this.digitalMagazineFormFields.magazinePDF);
+
+    if (this.digitalMagazineId) {
+      this.apiservice
+        .apiputcall(`magazines/${this.digitalMagazineId}`, formData)
+        .subscribe(results => {
+          Swal.fire(
+            'Updated!',
+            'Your Digital Magazine has been Updated.',
+            'success'
+          );
+          this.router.navigate(['/digital-magazine/list']);
+        });
+    } else {
+      this.apiservice.apipostcall('magazines', formData).subscribe(results => {
+        this.router.navigate(['/digital-magazine/list']);
+        Swal.fire(
+          'Added!',
+          'Your Digital Magazine has been Added.',
+          'success'
+        );
+      });
+    }
+  }
+
+  public onChangeThumbnailFile(files: FileList): void {
+    const fileReader = new FileReader();
+
+    if (files && files.length > 0) {
+      const thumbnailFile = files.item(0);
+      const thumbnailFileType: string = thumbnailFile.name.split('.').pop();
+
+      if (this.supportedImageFiles.includes(`.${thumbnailFileType}`)) {
+        fileReader.readAsText(thumbnailFile);
+        fileReader.onload = () => {
+          const file = files.item(0);
+          this.digitalMagazineFormFields.thumbnailImage = file;
+        };
+      } else {
+        const fileFormatError = `Only ${this.supportedImageFiles.join(
+          ', '
+        )} file is allowed`;
+        Swal.fire('Alert!', fileFormatError, 'warning');
+        this.digitalMagazineFormFields.thumbnailImage = '';
+      }
+    }
+  }
+
+  public onChangeMagazineFile(files: FileList): void {
+    const fileReader = new FileReader();
+
+    if (files && files.length > 0) {
+      const thumbnailFile = files.item(0);
+      const thumbnailFileType: string = thumbnailFile.name.split('.').pop();
+
+      if (this.supportedFiles.includes(`.${thumbnailFileType}`)) {
+        fileReader.readAsText(thumbnailFile);
+        fileReader.onload = () => {
+          const file = files.item(0);
+          this.digitalMagazineFormFields.magazinePDF = file;
+        };
+      } else {
+        const fileFormatError = `Only ${this.supportedFiles.join(
+          ', '
+        )} file is allowed`;
+        Swal.fire('Alert!', fileFormatError, 'warning');
+        this.digitalMagazineFormFields.magazinePDF = '';
+      }
+    }
+  }
+
+  private _getDigitalMagazineById(): void {
+    this.apiservice
+      .apigetcall(`magazines/${this.digitalMagazineId}`, {})
+      .subscribe(resp => {
+        if (resp) {
+          this.digitalMagazineFormFields = resp;
+        }
+      });
+  }
 }
